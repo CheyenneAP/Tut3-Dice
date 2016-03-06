c20eab0e2413a689ed5f78fce41bbc40814290af
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-06 16:41:13 +0200

    call the methods to display the dice outcomes and the roll number

--- Tut3-Dice/Tut3-Dice.cpp
+++ Tut3-Dice/Tut3-Dice.cpp
@@ -2,21 +2,22 @@
 #include <cmath>
 #include <ctime>
 using namespace std;
- 
+
 
 class Dice{
 private: 
-	int rollcount;
+	int rollcount,sum=0;
 	int outcome[6];
-	
+	float ave;
 
 public:
 	Dice(void);
 	int rollOutcome(void); // outcome on the dice 
 	int rollnum(); // number of times dice is rolled 
-	float average(Dice, int);
-	float average(int);
+	float average(Dice);
+	float average();
 	void display();
+	
 };
 
 Dice::Dice(){
@@ -40,44 +41,55 @@ int Dice::rollnum(){
 
 
 // overloading functions named average
-float Dice::average(Dice d, int numOutcome){
+float Dice::average(Dice d){
 	// obtain the average of all the dice rolls 
-	int sum;
-	float average;
-	sum = sum + numOutcome;
-	average = sum / rollcount;
-	return average;
+	sum = sum + rollOutcome();
+	ave = sum / rollcount;
+	return ave;
 }
-float Dice::average(int numOutcome){
-	// obstain the average of all the integers in the array
+float Dice::average(){
+	// obtain the average of all the integers in the array
 	for (int i = 0; i < rollcount; i++)
 	{
-		int sum;
-		float average;
-		outcome[i] = numOutcome;
 		sum = sum + outcome[i];
-		average = (sum / numOutcome);
-		return average;
+		ave= (float)(sum / rollcount);
+		
 	}
+	return ave;
 }
+
+
+
 void Dice::display(){
-
-
-	cout << rollcount << "\t rolls where made " << endl;
-	cout << " the outcome of the rolls are:";
-	for (int i = 0; i < rollcount; i++)
+	// displays the outcome with the dice roll
+	for (int i = 0; i < 6; i++)
 	{
-		cout << outcome[i];
+		int ro = rollOutcome();
+		outcome[i] = ro;
+		cout << "outcome\t" << i + 1 << "\t=\t" << ro << "\t Number of times Dice has rolled______" << rollnum();
+		cout << "\t\t\t Array[" << i << "] = " << outcome[i] << endl;
 	}
+
 }
+
+
 
 
 
 int main(){
 	Dice myDiObj;
-	cout << " Number of times Dice has rolled" << myDiObj.rollnum() << endl;
-	cout << "Dice outcomes for " << myDiObj.rollnum();
-	cout << "rolls is equal to " << endl;
-	
 
+
+	myDiObj.display();
+
+	 float a = myDiObj.average();
+	 cout << "average of the  outcomes  = " << a << endl;
+
+
+
+	
+	
+	
+	
+	
 }
